361cdc8f3747cafc82d40253164fbfe446135e3f
luthfisansan <luthfifauzan492@gmail.com>
2024-12-22 15:06:44 +0700

    Model seting

--- app/Models/BookingTransaction.php
+++ app/Models/BookingTransaction.php
@@ -1,10 +1,43 @@
 <?php
-
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BookingTransaction extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'name',
+        'phone_number',
+        'booking_trx',
+        'office_space_id',
+        'total_amount',
+        'duration',
+        'started_at',
+        'ended_at',
+        'is_paid'
+    ];
+
+    protected $casts = [
+        'started_at' => 'datetime',
+        'ended_at' => 'datetime',
+        'is_paid' => 'boolean'
+    ];
+    public static function generateUniqueTrxId()
+    {
+        $prefix = 'LFZN';
+        
+        do {
+            $randomString = $prefix . mt_rand(1000, 9999);
+        } while (self::where('booking_trx', $randomString)->exists());
+        
+        return $randomString;
+    }
+    public function officeSpace(): BelongsTo
+    {
+        return $this->belongsTo(OfficeSpace::class);
+    }
 }
--- app/Models/OfficeSpace.php
+++ app/Models/OfficeSpace.php
@@ -2,9 +2,53 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 class OfficeSpace extends Model
 {
-    //
+        use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'name',
+        'slug',
+        'thumbnail',
+        'about',
+        'city_id',
+        'is_open',
+        'is_full_booked',
+        'price',
+        'duration',
+        'address'
+    ];
+
+    protected $casts = [
+        'is_open' => 'boolean',
+        'is_full_booked' => 'boolean'
+    ];
+    public function setNameAttributes($value){
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function photos(): HasMany
+    {
+        return $this->hasMany(OfficeSpacePhoto::class);
+    }
+
+    public function benefits(): HasMany
+    {
+        return $this->hasMany(OfficeSpaceBenefit::class);
+    }
+
+    public function bookingTransactions(): HasMany
+    {
+        return $this->hasMany(BookingTransaction::class);
+    }
 }
